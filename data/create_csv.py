1d1f5e22eb19a2e94d027c847f03ed03c3cdcfb3
kiritharan <kiritharan@cmu.edu>
2020-04-15 00:11:49 -0400

    some renaming and styling stuff

--- data/create_csv.py
+++ data/create_csv.py
@@ -7,4 +7,5 @@ for i in range(329):
 	year = "1982"
 	page = i
 	# print("/var/www/html/drupal/web/modules/contrib/migrate_islandora_csv/data-cmu/images/thistle/%s/page_%s.jpg,thistle %s page %s,,page %s of thistle %s,%s,") % (year,page,year,page,page,year,year)
-	print("Paged/%s_THISTLE,Paged/%s_THISTLE/%s,%s,/var/www/html/drupal/web/modules/contrib/migrate_islandora_csv/data-cmu/images/thistle/%s/page_%s.jpg") % (year,year,page,page,year,page)
+	# print("Paged/%s_THISTLE,Paged/%s_THISTLE/%s,%s,/var/www/html/drupal/web/modules/contrib/migrate_islandora_csv/data-cmu/images/thistle/%s/page_%s.jpg") % (year,year,page,page,year,page)
+	print("%s_thistle|/var/www/html/drupal/web/modules/contrib/migrate_islandora_csv/data/images/thistle/%s/page_%s.jpg|thistle %s page %s||page %s of thistle %s|%s|%s") % (year,year,page,year,page,page,year,year,page)
