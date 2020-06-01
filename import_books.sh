a21259f2ad8bcc3112a2f1251645bdb7e273d0dc
kiritharan <kiritharan@cmu.edu>
2020-06-01 07:52:27 -0400

    update steps

--- import_books.sh
+++ import_books.sh
@@ -1,11 +1,16 @@
 echo "import paged content"
 
-# jk lets try to see what happens with OCR.
+# do taxonomy first
+drush -y --userid=1 --uri=localhost:8000 migrate:import cmu_taxonomy_corporate_bodies
+drush -y --userid=1 --uri=localhost:8000 migrate:import cmu_taxonomy_geo_locations
+drush -y --userid=1 --uri=localhost:8000 migrate:import cmu_taxonomy_subjects
 
-drush -y --userid=1 --uri=localhost:8000 migrate:import thistlepagefiles
+drush -y --userid=1 --uri=localhost:8000 migrate:import thistle_collection
+
+drush -y --userid=1 --uri=localhost:8000 migrate:import thistle_page_files
 # i don't think we need a media or file for the book.
-drush -y --userid=1 --uri=http://localhost:8000 migrate:import thistlebooknodes
+drush -y --userid=1 --uri=http://localhost:8000 migrate:import thistle_book_nodes
 
-drush -y --userid=1 --uri=http://localhost:8000 migrate:import thistlepagenodes
+drush -y --userid=1 --uri=http://localhost:8000 migrate:import thistle_page_nodes
 # to connect the files to the nodes we need to make media
-drush -y --userid=1 --uri=http://localhost:8000 migrate:import thisetlepagemedia
+drush -y --userid=1 --uri=http://localhost:8000 migrate:import thistle_page_media
